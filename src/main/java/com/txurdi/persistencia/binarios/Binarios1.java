bf4f8e3f3e864b5c9f6a642fedecbfdb5287e83b
Ibaiarri <ibai.lnd@gmail.com>
2022-10-06 08:16:22 +0200

    r

---
+++ src/main/java/com/txurdi/persistencia/binarios/Binarios1.java
@@ -0,0 +1,44 @@
+package com.txurdi.persistencia.binarios;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Binarios1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		FileOutputStream fos = null;
+		DataOutputStream salida = null;
+		int n;
+		try {
+
+			fos = new FileOutputStream("ficheros/fichero.dat");
+			salida = new DataOutputStream(fos);
+			System.out.print("Introduce número entero. -1 para acabar: ");
+			n = sc.nextInt();
+			while (n != -1) {
+				salida.writeInt(n); // se escribe el número entero en el fichero
+				System.out.print("Introduce número entero. -1 para acabar: ");
+				n = sc.nextInt();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if (fos != null) {
+					fos.close();
+				}
+				if (salida != null) {
+					salida.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}//main
+}
